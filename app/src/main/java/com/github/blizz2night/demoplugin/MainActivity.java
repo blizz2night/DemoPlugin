a6e24015aa3798817dcc681d112ac82feb369bb7
yulie.yan <yulie.yan@tinno.com>
2021-08-07 11:50:01 +0800

    first commit

---
+++ app/src/main/java/com/github/blizz2night/demoplugin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.blizz2night.demoplugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
